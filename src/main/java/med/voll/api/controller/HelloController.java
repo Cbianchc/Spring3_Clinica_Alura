cf24a5fa036732af44b88ab8db2f94a1284f1588
Cirob <bianchimaniciroe@gmail.com>
2023-10-03 19:47:06 -0300

    Segundo commit

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -3,7 +3,7 @@ package med.voll.api.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//Primera api en java  clinica medica.
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
